76141cd184a307e41b559d712ef60b2821096345
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-02-17 00:46:18 +0700

    add template

--- resources/views/livewire/student.blade.php
+++ resources/views/livewire/student.blade.php
@@ -1,3 +1,65 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+
+    <div class="container">
+        <!-- START FORM -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <form>
+                <div class="mb-3 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10"><button type="button" class="btn btn-primary" name="submit">SIMPAN</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- AKHIR FORM -->
+
+        <!-- START DATA -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <h1>Data Mahasiswa</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th class="col-md-1">No</th>
+                        <th class="col-md-4">Nama</th>
+                        <th class="col-md-3">Email</th>
+                        <th class="col-md-2">Alamat</th>
+                        <th class="col-md-2">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Muhammad</td>
+                        <td>muhammad@gmail.com</td>
+                        <td>Yogyakarta</td>
+                        <td>
+                            <a href="" class="btn btn-warning btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Del</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <!-- AKHIR DATA -->
+    </div>
+
 </div>
